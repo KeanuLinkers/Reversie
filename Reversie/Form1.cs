4ac8cc9d6d2a63535289c334b06dc6090ff5bba3
Keanu Linkers <k.a.linkers@students.uu.nl>
2021-12-21 16:40:34 +0100

    Add project files.

---
+++ Reversie/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reversie
+{
+    
+    public partial class Form1 : Form
+    {
+        int boardSize, circleDiameter;
+        int[,] board;
+        public Form1()
+        {
+            boardSize = 6;
+            board = new int[boardSize, boardSize];
+            InitializeComponent();
+            circleDiameter = panel1.Width / boardSize;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Brush bordkleur = new SolidBrush(Color.FromArgb(139, 69, 19));
+            Brush lijnkleur = new SolidBrush(Color.Black);
+            int xpositie, ypositie, breedte, hoogte;
+            xpositie = panel1.Location.X;
+            ypositie = panel1.Location.Y;
+            breedte = panel1.Width;
+            hoogte = panel1.Height;
+            e.Graphics.FillRectangle(bordkleur, xpositie, ypositie, breedte, hoogte);
+
+            e.Graphics.FillRectangle(lijnkleur, 35, 175, panel1.Width, 10);
+            e.Graphics.FillRectangle(lijnkleur, 35, 350, panel1.Width, 10);
+            e.Graphics.FillRectangle(lijnkleur, 35, 525, panel1.Width, 10);
+            e.Graphics.FillRectangle(lijnkleur, 233, 55, 10, panel1.Height);
+            e.Graphics.FillRectangle(lijnkleur, 467, 55, 10, panel1.Height);
+
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
