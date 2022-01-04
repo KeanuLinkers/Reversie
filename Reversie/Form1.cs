a324977e93cb35283d17e750848257d1be0d20cd
Keanu Linkers <k.a.linkers@students.uu.nl>
2022-01-04 18:57:01 +0100

    WIP, outlining ideas to check moves

--- Reversie/Form1.cs
+++ Reversie/Form1.cs
@@ -14,7 +14,7 @@ namespace Reversie
     public partial class Form1 : Form
     {
         int boardSize, circleDiameter, rowCount, colCount;         
-        bool boardCreated;
+        bool boardCreated, blueTurn;
         int aantal = 0;
 
 
@@ -23,10 +23,11 @@ namespace Reversie
      
         public Form1()
         {
-            boardSize = 4;
+            boardSize = 6;
             rowCount = boardSize;
             colCount = boardSize;
             board = new int[rowCount, colCount];
+            blueTurn = true;
             
             
             
@@ -44,11 +45,69 @@ namespace Reversie
                     }
                 }
             }
+
+            MarkLegalMoves(board);       
             boardCreated = false;
             InitializeComponent();            
-            circleDiameter = panel1.Width / boardSize;
-                     
+            circleDiameter = panel1.Width / boardSize;  
 
+        }
+
+        private bool IsEmptySpace(int i, int j)
+        {
+            return (board[i, j] == 0);
+        }
+
+        private bool CheckRow(int i, int j)
+        {
+            int k;
+            if ( i == 0 )
+            {
+                k = 0;
+                while (k < board.GetLength(0))
+                {
+
+                }
+            }
+        }
+
+        // look at adjacent spaces to find opponents color.
+        // if found, look in the direction of the adjacent space for your own color, to terminate the run mark space as a legal move.
+        private bool FindAdjacentRuns(int i, int j)
+        {
+            // several if-statements need to be inserted to check if we're on the edges of the board
+            // we shouldn't look past the board, there's probably a cleaner solution than a different if-statement for each corner/edge
+            if (i == 0)
+            {
+                if(blueTurn)
+                {
+
+                }
+            }
+        }
+
+        
+        private bool CheckLegalMove(int i, int j)
+        {
+            // if current space is empty
+            if (IsEmptySpace(i, j))
+            {
+                // look for runs in the adjacent spaces (up, down, left, right and diagonals)
+                FindAdjacentRuns(i, j);
+            }
+        }
+        
+        // if a space is a legal move for the current player, it is marked with a 3 in the array
+        private void MarkLegalMoves(int[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (CheckLegalMove(i, j))
+                        board[i, j] = 3;
+                }
+            }
         }
 
         private void panel1_MouseClick(object sender, MouseEventArgs e)
@@ -58,20 +117,20 @@ namespace Reversie
             g = e.X / circleDiameter;
             h = e.Y / circleDiameter;
 
-            if (aantal % 2 == 0)
+            if (blueTurn)
             {                        
                 board[g, h] = 1;
                 this.label1.Text = "Rood is aan zet";
                 this.label1.ForeColor = System.Drawing.Color.Red;
             }  
             
-            else if (aantal%2 != 0)
+            else
             {                             
                 board[g, h] = 2;
                 this.label1.Text = "Blauw is aan zet";
                 this.label1.ForeColor = System.Drawing.Color.Blue;
             }
-            aantal++;
+            blueTurn = !blueTurn;
             panel1.Invalidate();
 
         }
