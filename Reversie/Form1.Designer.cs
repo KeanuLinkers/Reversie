e2dfe82be501f553625e220e5451a6247cdf0902
Keanu Linkers <k.a.linkers@students.uu.nl>
2022-01-04 16:00:41 +0100

    bram code merged

--- Reversie/Form1.Designer.cs
+++ Reversie/Form1.Designer.cs
@@ -30,15 +30,28 @@ namespace Reversie
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(35, 55);
+            this.panel1.Location = new System.Drawing.Point(35, 130);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(601, 601);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Blauw is aan zet";
             // 
             // Form1
             // 
@@ -46,16 +59,19 @@ namespace Reversie
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlText;
             this.ClientSize = new System.Drawing.Size(800, 785);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Reversie";
+            this.Text = "Reversi";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
